4d53a8249379b083bd79951f827a12f5992030fd
unknown <bsse0803@iit.du.ac.bd>
2020-08-08 00:21:24 +0600

    create update machine page

--- Client/src/app/update-machine/update-machine.page.ts
+++ Client/src/app/update-machine/update-machine.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MachineService } from "../services/machine.service";
 @Component({
   selector: 'app-update-machine',
   templateUrl: './update-machine.page.html',
   styleUrls: ['./update-machine.page.scss'],
 })
 export class UpdateMachinePage implements OnInit {
-
-  constructor() { }
+  isChecked:boolean;
+  constructor(private machineService:MachineService) { }
 
   ngOnInit() {
+  }
+
+  togglePermission(){
+    console.log(this.isChecked);
+    
   }
 
 }
