add3a7901a5655b4b8b8f3fd985d8634757d3c24
unknown <bsse0803@iit.du.ac.bd>
2020-08-07 15:29:23 +0600

    get http req from reg machine

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -9,10 +9,13 @@ import { SuperTabsModule } from '@ionic-super-tabs/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), SuperTabsModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), SuperTabsModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
--- Client/src/app/reg-machine/reg-machine.page.ts
+++ Client/src/app/reg-machine/reg-machine.page.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { Plugins } from '@capacitor/core';
+
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-reg-machine',
   templateUrl: './reg-machine.page.html',
   styleUrls: ['./reg-machine.page.scss'],
 })
 export class RegMachinePage implements OnInit {
-  device;
   deviceInfo;
-  constructor()  {
-    const { Device } = Plugins;
-    this.device =Device; 
+  constructor(private httpClient: HttpClient)  {
+    let that = this;
+    this.deviceInfo =  this.httpClient.get('http://localhost:3000/machine').subscribe(response => {
+      that.deviceInfo = response;
+      console.log("Device info: ");
+      console.log(that.deviceInfo);
+      
+    });
     
   }
 
-  async ngOnInit() {
-    this.deviceInfo = await this.device.getInfo();
-    console.log(this.deviceInfo);
+   ngOnInit() {
+   
   }
 
   verifyMachine(){
